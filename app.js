58fe8e0e7866c26d167bdbc067076d761935c975
CollinK23 <kimballcollin@gmail.com>
2022-07-05 02:03:32 -0700

    commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-const navbar = document.querySelector('.navbar');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const navbar = document.querySelector(".navbar");
 
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-  navbar.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+  navbar.classList.toggle("active");
 });
 
-window.addEventListener("scroll", function() {
-  navbar.classList.toggle('while-scroll', window.scrollY > 0);
+window.addEventListener("scroll", function () {
+  navbar.classList.toggle("while-scroll", window.scrollY > 0);
 });
